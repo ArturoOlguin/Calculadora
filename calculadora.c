2b21617fbe123ad65d9a92dccb8fa7e15d09933d
ArturoOlguin <158232301+ArturoOlguin@users.noreply.github.com>
2024-02-22 12:52:13 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -4,3 +4,16 @@
 float suma (float num1, float num2){
     return (num1+num2);
 }
+
+
+int main (){
+
+float num1,num2;
+
+printf("Dame dos numeros");
+scanf ("%f", &num1);
+scanf("%f",&num2);
+
+printf("La suma es: %f", suma(num1, num2));
+
+}
