6febd00946023db89570f513f8486057c67daba5
Albertovdc <114888921+Albertovdc@users.noreply.github.com>
2024-02-22 12:59:14 -0600

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -19,12 +19,12 @@ float division(float n1,float n2){
 }
 int main (){
 
-float num1,num2;
-
-printf("Dame dos numeros");
-scanf ("%f", &num1);
-scanf("%f",&num2);
-
-printf("La suma es: %f", suma(num1, num2));
-resta(num1, num2);
+    float num1,num2;
+    
+    printf("Dame dos numeros");
+    scanf ("%f", &num1);
+    scanf("%f",&num2);
+    
+    printf("La suma es: %f", suma(num1, num2));
+    resta(num1, num2);
 }
