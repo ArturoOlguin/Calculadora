2ccade65e5befae3e75e33d7ae114425f5892dfd
ArturoOlguin <158232301+ArturoOlguin@users.noreply.github.com>
2024-02-22 13:14:16 -0600

    Update calculadora.c
    Programa completo

--- calculadora.c
+++ calculadora.c
@@ -25,16 +25,19 @@ float division(float num1,float num2){
     return (num1/num2);
   }
 }
+
 int main (){
 
     float num1,num2;
     
-    printf("Dame dos numeros");
+    printf("Dame dos numeros: \n");
     scanf ("%f", &num1);
     scanf("%f",&num2);
     
-    printf("La suma es: %.2f", suma(num1, num2));
-    printf("La resta es: %.2f",resta(num1, num2));
-    printf("La multiplicacion es: %.2f", multiplicaion(num1, num2));
-    printf("La division es: %.2f", division(num1, num2));
+    printf("La suma es: %.2f",suma(num1, num2));
+    printf("\nLa resta es: %.2f",resta(num1, num2));
+    printf("\nLa multiplicacion es: %.2f",multiplicacion(num1, num2));
+    printf("\nLa division es: %.2f",division(num1, num2));
+
+    return 0;
 }
